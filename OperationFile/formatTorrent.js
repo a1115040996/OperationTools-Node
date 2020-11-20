e2d98037acd8e105ec27b69942eb302a5dfd9582
a1115040996 <1115040996@qq.com>
2020-11-20 14:17:14 +0800

    添加parse-torrent

---
+++ OperationFile/formatTorrent.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-20 10:23:15
+ * @LastEditTime: 2020-11-20 10:53:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \OperationTools-Node\OperationFile\formatTorrent.js
+ */
+const fs = require('fs');
+const parseTorrent = require('parse-torrent');
+console.log('hello world  113');
---
+++ OperationFile/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-20 10:39:01
+ * @LastEditTime: 2020-11-20 13:56:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \OperationTools-Node\OperationFile\index.js
+ */
+require('./formatTorrent.js');
